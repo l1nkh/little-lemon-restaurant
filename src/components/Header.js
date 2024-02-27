c81cdbc5a20b4ce816c03901890826fec7a5d2e7
diogo-h <diogo.henriques@affinity.pt>
2024-02-27 17:41:57 +0000

    Minor changes for test purposes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-//import logo from logo './logo.svg';
+//import logo from './logo.svg';
 
 function Header(){
     return(
         <header>
-            <img src={logo} alt="Little Lemon Logo"></img>
+            <img src="" alt="Little Lemon Logo"></img>
         </header>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import logo from logo './logo.svg';
 
 function Main(){
     <main></main>
